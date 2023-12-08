9feea7990f14eba34f1fca33b516feaf39eb08df
Merve <babutine@yahoo.com>
2023-12-08 23:18:40 +0000

    feat: implement activity 5 and pass each cat property separately

--- src/App.tsx
+++ src/App.tsx
@@ -91,9 +91,14 @@ function App(): JSX.Element {
 
       <main>
         <div className="cards__wrapper">
-          {cats.map((cat) => {
-            return <CatCard catObject={cat} />;
-          })}
+          {cats.map((cat) => (
+            <CatCard
+              name={cat.name}
+              species={cat.species}
+              favFoods={cat.favFoods}
+              birthYear={cat.birthYear}
+            />
+          ))}
         </div>
       </main>
 
