4aaa1d5b7f8c68927deb41a0cae9bf0b1ae46f42
Merve <babutine@yahoo.com>
2023-12-08 20:41:34 +0000

    feat: implement activty 2 create catCard component

---
+++ src/components/cat_card.tsx
@@ -0,0 +1,10 @@
+const CatCard: React.FC = () => (
+  <div className="card">
+    <h3 className="card__text car__header">Mr Gwumpy</h3>a
+    <p className="card__text">Species: Cat</p>
+    <p className="card__text">Favourite Food: </p>
+    <p className="card__text">Birth Year: </p>
+  </div>
+);
+
+export default CatCard;
