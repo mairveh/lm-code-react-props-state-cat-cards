ecffed4d484d403eef125e435030180b68c1c7ec
Merve <babutine@yahoo.com>
2023-12-08 21:48:36 +0000

    feat: implement activity 4 and create a loop to render CatCard

--- src/App.tsx
+++ src/App.tsx
@@ -90,7 +90,9 @@ function App(): JSX.Element {
       <Header />
 
       <main>
-        <div className="cards__wrapper"><CatCard /></div>
+        <div className="cards__wrapper">
+				{cats.map(cat => <CatCard/>)}
+		</div>
       </main>
 
       <Footer />
